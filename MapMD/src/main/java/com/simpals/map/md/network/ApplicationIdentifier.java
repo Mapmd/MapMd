ed5f41d40467dfda9a3ba4c7dbce1ed5393296e3
Nicolae Cozma <koleacozma@gmail.com>
2019-07-15 19:54:17 +0300

    update map, upgrade api 29

--- MapMD/src/main/java/com/simpals/map/md/network/ApplicationIdentifier.java
+++ MapMD/src/main/java/com/simpals/map/md/network/ApplicationIdentifier.java
@@ -8,11 +8,8 @@ public class ApplicationIdentifier {
     }
 
     private void setToken(String accessToken) {
-        /*HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);*/
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new BasicAuthInterceptor(accessToken, ""))
-                /*.addInterceptor(interceptor)*/
                 .build();
         HttpRequestUtil.setOkHttpClient(client);
     }
