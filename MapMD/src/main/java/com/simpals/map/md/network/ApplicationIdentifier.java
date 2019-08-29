f132dbe312cfdbd3782d84776c748e6b55c1e8f9
Nicolae Cozma <koleacozma@gmail.com>
2019-08-29 21:13:08 +0300

    resolve network provider

--- MapMD/src/main/java/com/simpals/map/md/network/ApplicationIdentifier.java
+++ MapMD/src/main/java/com/simpals/map/md/network/ApplicationIdentifier.java
@@ -2,9 +2,16 @@ package com.simpals.map.md.network;
 import com.mapbox.mapboxsdk.module.http.HttpRequestUtil;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import okhttp3.CipherSuite;
 import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import static java.util.Collections.singletonList;
 
 public class ApplicationIdentifier {
     public ApplicationIdentifier(String accessToken) {
@@ -12,10 +19,15 @@ public class ApplicationIdentifier {
     }
 
     private void setToken(String accessToken) {
+      /*  HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);*/
+
         OkHttpClient client = new OkHttpClient.Builder()
                 /*.connectionSpecs(Arrays.asList(ConnectionSpec.COMPATIBLE_TLS))*/
                 .addInterceptor(new BasicAuthInterceptor(accessToken, ""))
+               // .addInterceptor(logging)
                 .build();
+
         HttpRequestUtil.setOkHttpClient(client);
     }
 }
